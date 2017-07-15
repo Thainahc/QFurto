402c841a8045d95292bc286924b9ce3211ec6f56
RodriguesDiego95 <diegosr_12@hotmail.com>
2017-07-14 21:49:26 -0300

    Classe Anuncio

---
+++ QFurto/Models/Entities/Anuncio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QFurto.Models.Entities
+{
+    public class Anuncio
+    {
+        public int AnuncioId { get; set; }
+        public DateTime DataHora { get; set; }
+        public string NomePessoa { get; set; }
+        public string Modelo { get; set; }
+        public string Placa { get; set; }
+        public int Ano { get; set; }
+        public string Descricao { get; set; }
+        public string Telefone { get; set; }
+        //public IMAGEM FotoVeiculo { get; set; }
+        //TipoVeiculo
+        //Bairro
+    }
+}
